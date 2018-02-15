f7030eb28d61a3e6f22f04861b62ad2a3fdf7201
Christophe Moinard <christophe.moinard@meteodyn.com>
2018-02-15 13:29:54 +0100

    Remove indexer

--- GildedRose/GildedRose/QualityUpdater.cs
+++ GildedRose/GildedRose/QualityUpdater.cs
@@ -10,75 +10,75 @@ namespace GildedRose
         
         public void UpdateQuality(IList<Item> items)
         {
-            for (var i = 0; i < items.Count; i++)
+            foreach (var item in items)
             {
-                if (items[i].Name != AgedBrie &&
-                    items[i].Name != Backstage)
+                if (item.Name != AgedBrie &&
+                    item.Name != Backstage)
                 {
-                    if (items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        if (items[i].Name != Sulfuras)
+                        if (item.Name != Sulfuras)
                         {
-                            items[i].Quality = items[i].Quality - 1;
+                            item.Quality = item.Quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    if (items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        items[i].Quality = items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
 
-                        if (items[i].Name == Backstage)
+                        if (item.Name == Backstage)
                         {
-                            if (items[i].SellIn < 11)
+                            if (item.SellIn < 11)
                             {
-                                if (items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    items[i].Quality = items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
 
-                            if (items[i].SellIn < 6)
+                            if (item.SellIn < 6)
                             {
-                                if (items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    items[i].Quality = items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (items[i].Name != Sulfuras)
+                if (item.Name != Sulfuras)
                 {
-                    items[i].SellIn = items[i].SellIn - 1;
+                    item.SellIn = item.SellIn - 1;
                 }
 
-                if (items[i].SellIn < 0)
+                if (item.SellIn < 0)
                 {
-                    if (items[i].Name != AgedBrie)
+                    if (item.Name != AgedBrie)
                     {
-                        if (items[i].Name != Backstage)
+                        if (item.Name != Backstage)
                         {
-                            if (items[i].Quality > 0)
+                            if (item.Quality > 0)
                             {
-                                if (items[i].Name != Sulfuras)
+                                if (item.Name != Sulfuras)
                                 {
-                                    items[i].Quality = items[i].Quality - 1;
+                                    item.Quality = item.Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            items[i].Quality = items[i].Quality - items[i].Quality;
+                            item.Quality = item.Quality - item.Quality;
                         }
                     }
                     else
                     {
-                        if (items[i].Quality < 50)
+                        if (item.Quality < 50)
                         {
-                            items[i].Quality = items[i].Quality + 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
